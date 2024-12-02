a85a82f15939c42802d88a31138966752fae183d
Taofeek250111 <164615598+Taofeek250111@users.noreply.github.com>
2024-12-02 23:26:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "2348165610315"
+global.ownername = "khalid"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
